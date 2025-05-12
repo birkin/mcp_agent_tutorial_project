8c8ba9a35a500abf60c4dd57fd0ac2e06375d011
Birkin James Diana <birkin.diana@gmail.com>
2025-05-12 11:06:51 -0400

    adds json template and script to create actual file.

---
+++ save_json.py
@@ -0,0 +1,24 @@
+"""
+Loads the json template, and saves it to the outer 'stuff' directory, replacing paths with envars.
+
+Usage: uv run ./save_json.py --env ../.env
+"""
+
+import json
+import os
+
+## load envars and raise error if not found -------------------------
+DB_PATH = os.environ['DB_PATH']
+OLLAMA_MCP_PATH = os.environ['OLLAMA_MCP_PATH']
+
+## load json template -----------------------------------------------
+with open('json_template.txt', 'r') as f:
+    template = json.load(f)
+
+## replace paths with envars -----------------------------------------
+template['mcpServers']['sqlite']['args'][2] = os.getenv('DB_PATH')
+template['mcpServers']['filesystem']['args'][2] = os.getenv('OLLAMA_MCP_PATH')
+
+## save actual json-file --------------------------------------------
+with open('../mcp_servers.json', 'w') as f:
+    json.dump(template, f, indent=2)
