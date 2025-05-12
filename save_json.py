c5acc9c36428ca13bfffe52e886af0d11255b7e4
Birkin James Diana <birkin.diana@gmail.com>
2025-05-12 15:57:23 -0400

    updates var name.

--- save_json.py
+++ save_json.py
@@ -9,15 +9,15 @@ import os
 
 ## load envars and raise error if not found -------------------------
 DB_PATH = os.environ['DB_PATH']
-OLLAMA_MCP_PATH = os.environ['OLLAMA_MCP_PATH']
+OLLAMA_MCP_FILESYSTEM_PATH = os.environ['OLLAMA_MCP_FILESYSTEM_PATH']
 
 ## load json template -----------------------------------------------
-with open('json_template.txt', 'r') as f:
+with open('json_template.json', 'r') as f:
     template = json.load(f)
 
 ## replace paths with envars -----------------------------------------
 template['mcpServers']['sqlite']['args'][2] = os.getenv('DB_PATH')
-template['mcpServers']['filesystem']['args'][2] = os.getenv('OLLAMA_MCP_PATH')
+template['mcpServers']['filesystem']['args'][2] = os.getenv('OLLAMA_MCP_FILESYSTEM_PATH')
 
 ## save actual json-file --------------------------------------------
 with open('../mcp_servers.json', 'w') as f:
